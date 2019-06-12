bc5e2f9e73b8570aa3c35aadec1230c0914c5521
Chris Elrod <elrodc@gmail.com>
2019-06-12 09:47:02 -0500

    Progress on (slowly) adding a multivariate normal; will probably change subtantially before it gets supported.

--- src/normal_variates.jl
+++ src/normal_variates.jl
@@ -42,14 +42,14 @@ end
 
 
 
-function CovarianceMatrix(
+@generated function CovarianceMatrix(
                 rhos, L::StructuredMatrices.AbstractLowerTriangularMatrix{K,T},
-                δₜ::ConstantFixedSizePaddedVector{nT}, workspace
-            ) where {K,T,nT}
+                δₜ::ConstantFixedSizePaddedVector{nTm1}, workspace
+            ) where {K,T,nTm1}
+    nT = nTm1 + 1
     Wm1 = VectorizationBase.pick_vector_width(nT, T) - 1
     nTl = (nT + Wm1) & ~Wm1
     quote
-        nT = length(δₜ) + 1
         # K = size(L,1)
         # ARs = workspace.ARs
         Base.Cartesian.@nexprs $K k -> AR_k = ConstantFixedSizePaddedMatrix(AutoregressiveMatrix(rhos[k], δₜ))
@@ -58,13 +58,13 @@ function CovarianceMatrix(
         # LowerTriangular
         Base.Cartesian.@nexprs $K kc -> begin
             for kr ∈ kc-1:K-1
-                Base.Cartesian.@nexprs kc j -> l_k = L[kr+1,j] * L[kc,j]
+                Base.Cartesian.@nexprs kc j -> l_j = L[kr+1,j] * L[kc,j]
                 # Sigview = @view Sigfull[(1:$nTl) .+ $nT*kr, (1:$nT) .+ $nT*(kc-1)]
                 sigrow, sigcol = $nT*kr, $nT*(kc-1)
                 for tc ∈ 1:$nT
                     @simd ivdep for tr ∈ 1:$nTl
-                        ari = l_1 * ARs_1[tr, tc]
-                        Base.Cartesian.@nexprs kc j -> ari = muladd(l_{j+1}, AR_{j+1}[tr, tc], ari)
+                        ari = l_1 * AR_1[tr, tc]
+                        Base.Cartesian.@nexprs kc-1 j -> ari = muladd(l_{j+1}, AR_{j+1}[tr, tc], ari)
                         Sigfull[ tr + sigrow, tc + sigcol] = ari
                     end
                 end
@@ -83,7 +83,7 @@ struct Covariance_LAR_L_Adjoint{K, nT, T, nTP, L, LKJ_T <: StructuredMatrices.Ab
     LKJ::LKJ_T
 end
 
-function ∂CovarianceMatrix(
+@generated function ∂CovarianceMatrix(
                 rhos::PaddedMatrices.AbstractFixedSizePaddedVector{K,T}, L::StructuredMatrices.AbstractLowerTriangularMatrix{K,T},
                 times::ConstantFixedSizePaddedVector{nT}, workspace, ::Val{(true,true)}
             ) where {K,T,nT}
@@ -94,34 +94,51 @@ function ∂CovarianceMatrix(
         # K = size(L,1)
         ARs = workspace.ARs
         ∂ARs = workspace.∂ARs
-        for k ∈ 1:k
+        for k ∈ 1:$K
             rho = rhos[k]
             if rho < 0
                 absrho = abs(rho)
                 for tc ∈ 1:nT
                     for tr ∈ 1:tc-1
+                        # sign = (-1)^(tc-tr)
+                        # deltatimes = times[tc] - times[tr]
+                        # rhot = sign* absrho^(deltatimes - one(T))
+                        # ARs[tr,tc,k] = rho*rhot
+                        # ∂ARs[tr,tc,k] = deltatimes*rhot
+                        ARs[tr,tc,k] = ARs[tc,tr,k]
+                        ∂ARs[tr,tc,k] = ∂ARs[tc,tr,k]
+                    end
+                    ARs[tc,tc,k] = one(T)
+                    ∂ARs[tc,tc,k] = zero(T)
+                    for tr ∈ tc+1:nT
                         sign = (-1)^(tc-tr)
-                        deltatimes = times[tc] - times[tr]
+                        deltatimes = times[tr] - times[tc]
                         rhot = sign* absrho^(deltatimes - one(T))
                         ARs[tr,tc,k] = rho*rhot
                         ∂ARs[tr,tc,k] = deltatimes*rhot
                     end
-                    ARs[tc,tc,k] = one(T)
-                    ∂ARs[tc,tc,k] = zero(T)
                 end
             else # rho > 0
                 for tc ∈ 1:nT
                     for tr ∈ 1:tc-1
-                        deltatimes = times[tc] - times[tr]
+                        # deltatimes = times[tc] - times[tr]
+                        # rhot = rho^(deltatimes - one(T))
+                        # ARs[tr,tc,k] = rho*rhot
+                        # ∂ARs[tr,tc,k] = deltatimes*rhot
+                        ARs[tr,tc,k] = ARs[tc,tr,k]
+                        ∂ARs[tr,tc,k] = ∂ARs[tc,tr,k]
+                    end
+                    ARs[tc,tc,k] = one(T)
+                    ∂ARs[tc,tc,k] = zero(T)
+                    for tr ∈ tc+1:nT
+                        deltatimes = times[tr] - times[tc]
                         rhot = rho^(deltatimes - one(T))
                         ARs[tr,tc,k] = rho*rhot
                         ∂ARs[tr,tc,k] = deltatimes*rhot
                     end
-                    ARs[tc,tc,k] = one(T)
-                    ∂ARs[tc,tc,k] = zero(T)
                 end
             end
-            ARs[:,:,k] .= AutoregressiveMatrix(rhos[k], δₜ)
+            # ARs[:,:,k] .= AutoregressiveMatrix(rhos[k], δₜ)
         end
         # Base.Cartesian.@nexprs $K k -> AR_k = ConstantFixedSizePaddedMatrix(AutoregressiveMatrix(rhos[k], δₜ))
         # ARs = [ConstantFixedSizePaddedMatrix(AutoregressiveMatrix(rho, δₜ)) for rho ∈ rhos]
@@ -129,13 +146,13 @@ function ∂CovarianceMatrix(
         # ∂Sig∂L
         Base.Cartesian.@nexprs $K kc -> begin
             for kr ∈ kc-1:K-1
-                Base.Cartesian.@nexprs kc j -> l_k = L[kr+1,j] * L[kc,j]
+                Base.Cartesian.@nexprs kc j -> l_j = L[kr+1,j] * L[kc,j]
                 # Sigview = @view Sigfull[(1:$nTl) .+ $nT*kr, (1:$nT) .+ $nT*(kc-1)]
                 sigrow, sigcol = $nT*(kc-1), $nT*kr # transpose it
                 for tc ∈ 1:$nT
                     @simd ivdep for tr ∈ 1:$nTl
-                        ari = l_1 * ARs_1[tr, tc]
-                        Base.Cartesian.@nexprs kc j -> ari = muladd(l_{j+1}, AR_{j+1}[tr, tc], ari)
+                        ari = l_1 * ARs[tr, tc, 1]
+                        Base.Cartesian.@nexprs kc-1 j -> ari = muladd(l_{j+1}, ARs[tr, tc, j+1], ari)
                         Sigfull[ tr + sigrow, tc + sigcol] = ari
                     end
                 end
@@ -152,7 +169,7 @@ end
 
     Wm1 = VectorizationBase.pick_vector_width(T)-1
     KL = (K+Wm1) & ~Wm1
-    outtup = Expr(:tuple, [:(κ_$k) for k ∈ 1:K]..., [zero(T) for k ∈ K+1:KL]...)
+    outtup = Expr(:tuple, [Symbol(:κ_,k) for k ∈ 1:K]..., [zero(T) for k ∈ K+1:KL]...)
     quote
         $(Expr(:meta,:inline))
         # Calculate all in 1 pass?
@@ -160,6 +177,7 @@ end
         # Cstride = stride(C,2)
         LKJ = adj.LKJ
         ∂ARs = adj.∂ARs
+        Base.Cartesian.@nexprs $K k -> κ_k = zero(T)
         Base.Cartesian.@nexprs $K kc -> begin
             ccoloffset = (kc-1)*$nT
 
@@ -196,65 +214,107 @@ end
 
     end
 end
+ 
+# function calc_lkj_partial(ar, ::Val{K}, ::Val{KC}) where {K, KC}
 
-@generated function Base.:*(C::AbstractMatrix{T},
-                adj::Covariance_LAR_L_Adjoint{K, nT, T, nTP}
+# end
+
+# @generated function Base.:*(C::AbstractMatrix{T},
+#                     adj::Covariance_LAR_L_Adjoint{K, nT, T, nTP}
+#                 ) where {K, nT, T, nTP}
+
+#     ∂LKJ = StructuredMatrices.MutableLowerTriangularMatrix{K,T}(undef)
+#     quote
+#         # $(Expr(:meta,:inline))
+
+#         # ∂LKJ = MutableLowerTriangualrMatrix{$K,$T}(undef)
+#         ∂LKJ = $∂LKJ
+#         LKJ = adj.LKJ
+#         ARs = adj.ARs
+
+#         for kc ∈ 1:$K
+#             # coffset = (kc-1)*stride(C,2)
+#             # AR[:,:,kc]
+#             @inbounds for kr ∈ kc:$K
+#                 ∂lkj = zero(T)
+#                 for lr ∈ kc:kr-1
+#                     lkj = LKJ[lr,kc]
+#                     for tc ∈ 1:$nT
+#                         # C: kr = row, lr = col
+#                         @fastmath for tr ∈ 1:$nT
+#                             ∂lkj += lkj * ARs[tr,tc,kc] * C[tr + (kr-1)*$nT,tc + (lr-1)*$(K*nT)]
+#                         end
+#                     end
+#                 end
+#                 lkj = 2LKJ[kr,kc]
+#                 for tc ∈ 1:$nT
+#                     # C: kr = row, lr = col
+#                     @fastmath for tr ∈ 1:$nT
+#                         ∂lkj += lkj * ARs[tr,tc,kc] * C[tr + (kr-1)*$nT,tc + (kr-1)*$(K*nT)]
+#                     end
+#                 end
+#                 for lr ∈ kr+1:$K
+#                     lkj = LKJ[lr,kc]
+#                     for tc ∈ 1:$nT
+#                         # C: lr = row, kr = col
+#                         @fastmath for tr ∈ 1:$nT
+#                             ∂lkj += lkj * ARs[tr,tc,kc] * C[tr + (kr-1)*$nT,tc + (kr-1)*$(K*nT)]
+#                         end
+#                     end
+#                 end
+#                 ∂LKJ[kr,kc] = ∂lkj
+#             end
+#         end
+#         ∂LKJ
+#     end
+# end
+
+function Base.:*(C::AbstractMatrix{T},
+                    adj::Covariance_LAR_L_Adjoint{K, nT, T, nTP}
                 ) where {K, nT, T, nTP}
 
-    ∂LKJ = MutableLowerTriangualrMatrix{K,T}(undef)
-    quote
+    ∂LKJ = StructuredMatrices.MutableLowerTriangularMatrix{K,T}(undef)
+    
         # $(Expr(:meta,:inline))
 
         # ∂LKJ = MutableLowerTriangualrMatrix{$K,$T}(undef)
-        ∂LKJ = $∂LKJ
+        # ∂LKJ = $∂LKJ
+    LKJ = adj.LKJ
+    ARs = adj.ARs
 
-        for kc ∈ 1:$K
-            for kr ∈ kc:$K
-                AR[:,:,kc]
-                for lr ∈ kc:kr
-                    LKJ[lr,kc]
-                end
-                for lr ∈ kr+1:KL
-                    LKJ[lr,kc]
-                end
-
-            end
-        end
-
-
-
-        Base.Cartesian.@nexprs $K kc -> begin
-            ccoloffset = (kc-1)*$nT
-
-            kr = kc # diagonal block
-            l_ki_kj = LKJ[kr,kc]
-            @inbounds for tc ∈ 1:nT
-                ctcoloffset = ccoloffset + tc
-                crowoffset = (kr-1)*$nT
-                for tr ∈ tc:nT
-                    cij = C[tr + crowoffset, ctcoloffset]
-                    Base.Cartesian.@nexprs kc k -> begin
-                        κ_k = Base.FastMath.add_fast(Base.FastMath.mul_fast(cij,l_ki_kj,∂ARs[tr,tc,k]), κ_k)
+    for kc ∈ 1:K
+        # coffset = (kc-1)*stride(C,2)
+        # AR[:,:,kc]
+        @inbounds for kr ∈ kc:K
+            ∂lkj = zero(T)
+            for lr ∈ kc:kr-1
+                lkj = LKJ[lr,kc]
+                for tc ∈ 1:nT
+                    # C: kr = row, lr = col
+                    @fastmath for tr ∈ 1:nT
+                        ∂lkj += lkj * ARs[tr,tc,kc] * C[tr + (kr-1)*nT,tc + (lr-1)*(nT)]
                     end
                 end
             end
-
-            # offdiagonal block
-            @inbounds for tc ∈ 1:nT
-                ctcoloffset = ccoloffset + tc
-                for kr ∈ kc+1:K
-                    l_ki_kj = LKJ[kr,kc]
-                    crowoffset = (kr-1)*$nT
-                    for tr ∈ 1:nT
-                        cij = C[tr + crowoffset, ctcoloffset]
-                        Base.Cartesian.@nexprs kc k -> begin
-                            κ_k = Base.FastMath.add_fast(Base.FastMath.mul_fast(cij,l_ki_kj,∂ARs[tr,tc,k]), κ_k)
-                        end
+            lkj = 2LKJ[kr,kc]
+            for tc ∈ 1:nT
+                # C: kr = row, lr = col
+                @fastmath for tr ∈ 1:nT
+                    ∂lkj += lkj * ARs[tr,tc,kc] * C[tr + (kr-1)*nT,tc + (kr-1)*(nT)]
+                end
+            end
+            for lr ∈ kr+1:K
+                lkj = LKJ[lr,kc]
+                for tc ∈ 1:nT
+                    # C: lr = row, kr = col
+                    @fastmath for tr ∈ 1:nT
+                        ∂lkj += lkj * ARs[tr,tc,kc] * C[tr + (kr-1)*nT,tc + (kr-1)*(nT)]
                     end
                 end
             end
+            ∂LKJ[kr,kc] = ∂lkj
         end
-        ∂LKJ
     end
+    ∂LKJ
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,73 +1,104 @@
 using DistributionParameters
 using Test
-
-
-using StaticArrays, SLEEF, BenchmarkTools, LoopVectorization
-
-@inline function my_dot(x, y)
-    out = zero(promote_type(eltype(x),eltype(y)))
-    @inbounds @fastmath for i ∈ eachindex(x, y)
-        out += x[i] * y[i]
-    end
-    out
-end
-function my_exp_dot_test(x, y)
-    w = MArray(x)
-    z = MArray(y)
-    @inbounds for i ∈ eachindex(w, z)
-        w[i] = SLEEF.exp(w[i])
-        z[i] = SLEEF.exp(z[i])
-    end
-    # x = SArray(w)
-    # y = SArray(z)
-    my_dot(w, z)
-    # x' * y
-end
-
-function my_exp_dot_test2(x, y)
-    w = MArray(x)
-    z = MArray(y)
-    @vectorize for i ∈ eachindex(w)
-        w[i] = exp(w[i])
-        z[i] = exp(z[i])
-    end
-    x = SArray(w)
-    y = SArray(z)
-    my_dot(x, y)
-end
-function my_exp_dot_test3(a::MVector{32,T}) where {T}
-    w = MVector{16,T}(undef)
-    @inbounds for i ∈ 1:16
-        w[i] = a[i]
-    end
-    z = MVector{16,T}(undef)
-    @inbounds for i ∈ 1:16
-        z[i] = a[i+16]
-    end
-    @inbounds for i ∈ eachindex(w, z)
-        w[i] = SLEEF.exp(w[i])
-        z[i] = SLEEF.exp(z[i])
-    end
-    x = SArray(w)
-    y = SArray(z)
-    my_dot(x, y)
-end
-
-function fastmul(A::PaddedMatrices.AbstractConstantFixedSizePaddedMatrix{M,N,T},
-                B::PaddedMatrices.AbstractConstantFixedSizePaddedMatrix{N,P,T}) where {M,N,P,T}
-    W = MutableFixedSizePaddedMatrix(A)
-    X = MutableFixedSizePaddedMatrix(B)
-    C = MutableFixedSizePaddedMatrix{M,P,T}(undef)
-    mul!(C, W, X)
-    ConstantFixedSizePaddedMatrix(C)
-end
+using ForwardDiff, LinearAlgebra, PaddedMatrices, StructuredMatrices, Random, ProbabilityModels,   
+        ProbabilityModels, LoopVectorization, VectorizationBase, SLEEFPirates, SIMDPirates
 
 @testset "DistributionParameters.jl" begin
     # Write your own tests here.
 
+K = 4
+S = (@Constant randn(K,4K)) |> x -> x * x'
+S *= (1/16)
+pS = StructuredMatrices.SymmetricMatrixL(S)
+L = StructuredMatrices.lower_chol(S);
 
-    inv_logit(x) = 1 / (1 + exp(-x))
-    ∂inv_logit(x) = (ilx = inv_logit(x); ilx * (1-ilx))
-    log∂inv_logit(x) = log(∂inv_logit(x))
+T = 8; δₜ = (1/16) * reduce(+, (@Constant randexp(T-1)) for i ∈ 1:8)
+times = vcat(zero(ConstantFixedSizePaddedVector{1,Float64}), cumsum(δₜ));
+ρ = ConstantFixedSizePaddedVector{4}((0.5,0.55,0.60,0.65));
+
+workspace = (
+    Sigfull = PaddedMatrices.PaddedArray{Float64}(undef, (K*T,K*T)),
+    ARs = PaddedMatrices.MutableFixedSizePaddedArray{Tuple{T,T,K},Float64}(undef),
+    ∂ARs = PaddedMatrices.MutableFixedSizePaddedArray{Tuple{T,T,K},Float64}(undef)
+);
+Cov, pAR, pL = DistributionParameters.∂CovarianceMatrix(ρ, L, times, workspace, Val((true,true)))
+
+Cov1s = PaddedMatrices.PaddedArray{Float64}(undef, (K*T,K*T)); Cov1s .= 1;
+
+
+
+
+
+vunstable(x::AbstractVector{T}) where {T} = ConstantFixedSizePaddedArray{Tuple{length(x)},T,1}(ntuple(i -> x[i], length(x)))
+x = randn(16,16);
+ltx = LowerTriangular(x);
+ltxv = vec(ltx);
+
+
+
+function trisub2(N)
+    out = collect(1:(N+1):N^2)
+    ind = 0
+    for n ∈ 0:N-1
+        ind += n+1
+        r = N-n-1
+        for i ∈ 1:r
+            ind += 1
+            push!(out, ind)
+        end
+    end
+    out
+end
+ltxv2 = ltx[trisub2(16)];
+trivec = vunstable(ltxv2);
+
+r2 = @Constant randn(120);
+@generated function load_lkj(a::PaddedMatrices.AbstractFixedSizePaddedVector{L,T}) where {L,T}
+    fp = quote end
+    sp = quote end
+    out = gensym(:L)
+    M = (Int(sqrt(1 + 8L))-1)>>1
+    DistributionParameters.load_parameter(fp.args, sp.args, out, DistributionParameters.LKJ_Correlation_Cholesky{M+1}, false)
+    quote
+        $(Symbol("##θparameter##")) = VectorizationBase.vectorizable(a);
+        target = zero($T)
+        $fp
+        $sp
+        $out, target
+    end
+end
+@generated function ∂load_lkj(a::PaddedMatrices.AbstractFixedSizePaddedVector{L,T}, seedout = (M = (Int(sqrt(1 + 8L))-1)>>1; fill(1.0, PaddedMatrices.Static{(((M+2)*(M+1))>>1)}()))) where {L,T}
+    fp = quote end
+    sp = quote end
+    out = gensym(:L)
+    M = (Int(sqrt(1 + 8L))-1)>>1
+    DistributionParameters.load_parameter(fp.args, sp.args, out, DistributionParameters.LKJ_Correlation_Cholesky{M+1}, true)
+    quote
+        $(Symbol("##∂θparameter##m")) = PaddedMatrices.MutableFixedSizePaddedVector{L,T}(undef);
+        $(Symbol("##∂θparameter##")) = VectorizationBase.vectorizable($(Symbol("##∂θparameter##m")))
+        $(Symbol("##θparameter##")) = VectorizationBase.vectorizable(a);
+        target = zero($T)
+        $fp
+        $(Symbol("###seed###", out)) = seedout'
+        $sp
+        $out, target, $(Symbol("##∂θparameter##m"))
+    end
+end
+Lm, t, partial = ∂load_lkj(r2, trivec); Lm
+
+r2v = Array(r2);
+nr2vl = @. 1 / (1 + exp(r2v));
+r2vl = 1 .- nr2vl;
+zvals = @. 1 - 2 * nr2vl;
+r2vlpartial = @. r2vl * nr2vl;
+r2l = vunstable(zvals);
+
+lkj, lkjac, lkjldgrad, lkjgrad = DistributionParameters.∂lkj_constrain(r2l);
+j = ForwardDiff.jacobian(x -> Array(DistributionParameters.lkj_constrain(vunstable(x))[1]), r2l);
+ltxvj = ltxv' * j;
+@test all(ltxvj .≈ trivec' * lkjgrad) # matches
+g = ForwardDiff.gradient(x -> DistributionParameters.lkj_constrain(vunstable(x))[2], r2l); g'
+@test all(lkjldgrad .≈ g)
+
 
 end
