7868b2d7168694c3bdeaa495990f3a37402c5f17
Chris Elrod <elrodc@gmail.com>
2019-12-03 16:51:36 -0500

    Removed some precompile statements.

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,12 +1,12 @@
 function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
     precompile(Tuple{typeof(DistributionParameters.constrain_lkj_factor_jac_quote),Int64,Int64,Int64,Type{Float64},Symbol,Symbol,Symbol,Symbol})
-    precompile(Tuple{DistributionParameters.var"##s40#19",Any,Any,Any,Any,Any,Any,Any,Any})
-    precompile(Tuple{DistributionParameters.var"##s40#33",Any,Any,Any,Any,Any,Any,Any,Any,Any,Any,Any})
+    # precompile(Tuple{DistributionParameters.var"##s40#19",Any,Any,Any,Any,Any,Any,Any,Any})
+    # precompile(Tuple{DistributionParameters.var"##s40#33",Any,Any,Any,Any,Any,Any,Any,Any,Any,Any,Any})
     precompile(Tuple{typeof(DistributionParameters.load_parameter!),Array{Any,1},Array{Any,1},Symbol,Type{RealArray{Tuple{12,8},B,T,2,Tuple{1,12},L,U} where U where L where T where B},Bool,Module,Symbol,Bool,Bool})
     precompile(Tuple{typeof(DistributionParameters.load_parameter!),Array{Any,1},Array{Any,1},Symbol,Type{CorrCholesky{8,T,L} where L where T},Bool,Module,Symbol,Bool,Bool})
     precompile(Tuple{typeof(DistributionParameters.corr_cholesky_adjoint_mul_quote),Int64,Type,Bool})
-    precompile(Tuple{DistributionParameters.var"##s40#32",Any,Any,Any,Any,Any,Any,Any})
+    # precompile(Tuple{DistributionParameters.var"##s40#32",Any,Any,Any,Any,Any,Any,Any})
     precompile(Tuple{typeof(DistributionParameters.constrain_lkj_factor_quote),Int64,Int64,Int64,Type{Float64},Symbol,Symbol})
-    precompile(Tuple{DistributionParameters.var"##s40#4",Any,Any,Any,Any,Any,Any,Any,Any,Any})
+    # precompile(Tuple{DistributionParameters.var"##s40#4",Any,Any,Any,Any,Any,Any,Any,Any,Any})
 end
