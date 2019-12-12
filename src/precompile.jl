d52235642d09f9749be6c78722e2762ab4f44712
Chris Elrod <elrodc@gmail.com>
2019-12-12 08:27:12 -0500

    Updated precompile.

--- src/precompile.jl
+++ src/precompile.jl
@@ -5,4 +5,8 @@ function _precompile_()
     precompile(Tuple{typeof(DistributionParameters.corr_cholesky_adjoint_mul_quote),Int64,Type,Bool})
     precompile(Tuple{typeof(DistributionParameters.load_parameter!),Array{Any,1},Array{Any,1},Symbol,Type{CorrCholesky{8,T,L} where L where T},Bool,Module,Symbol,Bool,Bool})
     precompile(Tuple{typeof(DistributionParameters.load_parameter!),Array{Any,1},Array{Any,1},Symbol,Type{RealArray{Tuple{12,8},B,T,2,Tuple{1,12},L,U} where U where L where T where B},Bool,Module,Symbol,Bool,Bool})
+    precompile(Tuple{typeof(DistributionParameters.load_parameter!),Array{Any,1},Array{Any,1},Symbol,Type{RealArray{Tuple{8},B,T,1,Tuple{1},L,U} where U where L where T where B},Bool,Module,Symbol,Bool,Bool})
+    precompile(Tuple{typeof(DistributionParameters.load_parameter!),Array{Any,1},Array{Any,1},Symbol,Type{RealArray{Tuple{8},Bounds{Float64}(0.0, Inf),T,1,Tuple{1},L,U} where U where L where T},Bool,Module,Symbol,Bool,Bool})
+    precompile(Tuple{typeof(PaddedMatrices.type_length),Type{CorrCholesky{8,T,L} where L where T}})
+    precompile(Tuple{typeof(hash),Pair{Int64,Bounds{Float64}},UInt64})
 end
